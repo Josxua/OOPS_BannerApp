68aca34a7d336c44edd3e1e4ea8817e137b530cf
Joshua Thomas Joseph <joshua@Joshuas-MacBook-Pro-2.local>
2026-02-23 13:22:01 +0530

    OOPS as banner (string array)

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,17 @@
 public class OOPSBannerApp{
     public static void main(String args[]){
-        System.out.println(String.join("   **    ","   **    ","*******  ","  *****  "));
-        System.out.println(String.join(" **  **  "," **  **  ","**     **"," **   ** "));
-        System.out.println(String.join("**    ** ","**    ** ","**     **"," **      "));
-        System.out.println(String.join("**    ** ","**    ** ","*******  ","  *****  "));
-        System.out.println(String.join("**    ** ","**    ** ","**       ","      ** "));
-        System.out.println(String.join(" **  **  "," **  **  ","**       "," **   ** "));
-        System.out.println(String.join("   **    ","   **    ","**       ","  *****  "));
+        String[] Lines= new String[7]; 
+        Lines[0]=String.join("","   **    ","   **    ","*******  ","  *****  ");
+        Lines[1]=String.join(""," **  **  "," **  **  ","**     **"," **   ** ");
+        Lines[2]=String.join("","**    ** ","**    ** ","**     **"," **      ");
+        Lines[3]=String.join("","**    ** ","**    ** ","*******  ","  *****  ");
+        Lines[4]=String.join("","**    ** ","**    ** ","**       ","      ** ");
+        Lines[5]=String.join(""," **  **  "," **  **  ","**       "," **   ** ");
+        Lines[6]=String.join("","   **    ","   **    ","**       ","  *****  ");
+
+        for(String Line : Lines)
+        {
+            System.out.println(Line);
+        }
     }
 }
